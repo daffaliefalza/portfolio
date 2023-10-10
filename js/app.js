c39b427fc6f240d70d18ff2ea8776acfd9444194
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2023-10-11 02:15:30 +0700

    fixing spacing

--- js/app.js
+++ js/app.js
@@ -2,42 +2,42 @@
 ScrollReveal().reveal(".about__title", {
   delay: 700,
   origin: "top",
-  distance: "50px",
+  distance: "80px",
 });
 ScrollReveal().reveal(".about__img-profile", {
   delay: 700,
   origin: "left",
-  distance: "50px",
+  distance: "80px",
 });
 
 ScrollReveal().reveal(".about__description", {
   delay: 500,
   origin: "right",
-  distance: "50px",
+  distance: "80px",
 });
 
 ScrollReveal().reveal(".skills__title", {
   delay: 700,
   origin: "top",
-  distance: "50px",
+  distance: "80px",
 });
 
 ScrollReveal().reveal(".skills-list", {
   delay: 700,
   origin: "bottom",
-  distance: "50px",
+  distance: "80px",
 });
 
 ScrollReveal().reveal(".projects__title", {
   delay: 700,
   origin: "top",
-  distance: "50px",
+  distance: "80px",
 });
 
 ScrollReveal().reveal(".projects .grid-container", {
   delay: 700,
   origin: "bottom",
-  distance: "50px",
+  distance: "80px",
 });
 
 const btnToggleTop = document.querySelector(".top");
